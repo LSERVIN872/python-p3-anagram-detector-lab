1bd48e721fff3935ec8af82bd0c534e0f1c7b2f3
luis <servinluis21@yahoo.com>
2024-01-12 09:11:10 -0500

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,19 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, possible_anagrams):
+        word_lower = self.word.lower()
+        matches = []
+
+        for anagram in possible_anagrams:
+            anagram_lower = anagram.lower()
+
+            if len(word_lower) != len(anagram_lower):
+                continue
+
+            if sorted(word_lower) == sorted(anagram_lower):
+                matches.append(anagram)
+
+        return matches
